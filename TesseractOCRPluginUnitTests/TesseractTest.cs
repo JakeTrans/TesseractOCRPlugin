9651464fef9e46e5709a6f688e22aabb96ca1268
JakeTrans <Jake_Damner@Yahoo.co.uk>
2020-12-17 14:17:09 +0000

    Tests Added

--- TesseractOCRPluginUnitTests/TesseractTest.cs
+++ TesseractOCRPluginUnitTests/TesseractTest.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using TesseractOCRPlugin;
 
 namespace TesseractOCRPluginUnitTests
@@ -7,10 +10,33 @@ namespace TesseractOCRPluginUnitTests
     [TestClass]
     public class TesseractTest
     {
+        private string startupPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase).Replace(@"file:\", string.Empty);
+
         [TestMethod]
         public void InitTest()
         {
             TesseractOCR TessOCR = new TesseractOCR("eng", TesseractOCR.Quality.High);
+        }
+
+        [TestMethod]
+        public void ScanTest()
+        {
+            string expectedtext = @"This is a lot of 12 point text to test the ocr code and see if it works on all types
+    of file format.
+
+    The quick brown dog jumped over the
+    lazy fox. The quick brown dog jumped
+    over the lazy fox. The quick brown dog
+    jumped over the lazy fox. The quick
+    brown dog jumped over the lazy fox.";
+
+            TesseractOCR TessOCR = new TesseractOCR("eng", TesseractOCR.Quality.High);
+
+            string text = TessOCR.OCRimage(startupPath + @"\Images\testocr.png");
+            Trace.WriteLine("Output of OCR was:");
+            Trace.WriteLine(text);
+
+            Assert.AreEqual(expectedtext.Replace(" ", "").Replace(Environment.NewLine, ""), text.Replace(" ", "").Replace("\n", ""), "OCR doesn't match");
         }
     }
 }
