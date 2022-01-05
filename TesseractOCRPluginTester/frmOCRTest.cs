f057efe24e2be2b1a5ece7e1950d74b809b624a2
JakeTrans <Jake_Damner@Yahoo.co.uk>
2022-01-05 18:03:32 +0000

    comments added

--- TesseractOCRPluginTester/frmOCRTest.cs
+++ TesseractOCRPluginTester/frmOCRTest.cs
@@ -6,13 +6,16 @@ namespace TesseractOCRPluginTester
 
         public frmOCRTest()
         {
+            //startup of OCR
             TessOCR = new TesseractOCRPlugin.TesseractOCR("eng", TesseractOCRPlugin.TesseractOCR.Quality.High);
             InitializeComponent();
         }
 
         private void btnSelectFile_Click(object sender, EventArgs e)
         {
+            //show Dialog
             FileSelector.ShowDialog();
+            //Run OCR
             RTBResult.Text = TessOCR.OCRimage(FileSelector.FileName);
         }
     }
