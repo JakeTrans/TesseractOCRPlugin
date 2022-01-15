2c74847275cfed8ecb1f0dfb9aab54b1671ad860
JakeTrans <Jake_Damner@Yahoo.co.uk>
2022-01-15 14:44:33 +0000

    Avalonia Testing form

--- TesseractPluginTester/MainWindow.axaml.cs
+++ TesseractPluginTester/MainWindow.axaml.cs
@@ -2,14 +2,26 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace TesseractPluginTester
 {
     public partial class MainWindow : Window
     {
+        private TesseractOCRPlugin.TesseractOCR TessOCR;
+
+        public OCRResults OCRData { get; set; }
+
         public MainWindow()
         {
+            OCRData = new OCRResults();
             InitializeComponent();
+            this.DataContext = OCRData;
+            TessOCR = new TesseractOCRPlugin.TesseractOCR("eng", TesseractOCRPlugin.TesseractOCR.Quality.High);
+
 #if DEBUG
             this.AttachDevTools();
 #endif
@@ -22,12 +34,39 @@ namespace TesseractPluginTester
 
         private void RunOCR_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Debug.Print("test");
+            OCRData.OCRResulttext = TessOCR.OCRimage(OCRData.FilePath);
+            OCRData.OnPropertyChanged(nameof(OCRData.OCRResulttext));
         }
 
-        private void Browse_Click(object sender, RoutedEventArgs e)
+        private async void Browse_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Debug.Print("test");
+            OCRData.FilePath = await GetFilepath();
+            OCRData.OnPropertyChanged(nameof(OCRData.FilePath));
         }
+
+        public async Task<string> GetFilepath()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            // dialog.Filters.Add(new FileDialogFilter() { Name = "Text", Extensions = { "txt" } });
+
+            string[] result = await dialog.ShowAsync(this);
+
+            if (result != null)
+            {
+                string.Join(" ", "");
+            }
+
+            return string.Join(" ", result);
+        }
+    }
+
+    public class OCRResults : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string? FilePath { get; set; }
+        public string? OCRResulttext { get; set; }
+
+        public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
