a14d1eac4cef75f2e677c6dfeb4f82ffbd8bfde3
Jake Trans <Jake_Damner@yahoo.co.uk>
2024-03-29 17:38:45 +0000

    Deskew commenting and a cleanup up

--- TesseractOCRPlugin (.NET Standard)/Deskew/Deskew.cs
+++ TesseractOCRPlugin (.NET Standard)/Deskew/Deskew.cs
@@ -19,13 +19,14 @@ namespace TesseractOCRPlugin__.Net_Standard_.Deskew
             // Convert to grayscale
             Mat gray = new Mat();
             Mat Bilateral = new Mat();
+            //Grayscale Image
             CvInvoke.CvtColor(src, gray, ColorConversion.Bgr2Gray);
-
+            //Apply Smoothing to the image
             CvInvoke.BilateralFilter(gray, Bilateral, 15, 10, 10);
-
+            //use Canny  to focus the image
             Mat cannyEdges = new Mat();
             CvInvoke.Canny(Bilateral, cannyEdges, cannyThreshold, cannyThresholdLinking);
-
+            //get Hough Lines results
             LineSegment2D[] lines = CvInvoke.HoughLinesP(
                 image: cannyEdges,
                 rho: 1, //Distance resolution in pixel-related units
@@ -34,16 +35,8 @@ namespace TesseractOCRPlugin__.Net_Standard_.Deskew
                 30, //min Line width
                 10); //gap between lines
 
-            //LineSegment2D[] lines = CvInvoke.HoughLinesP(
-            //    image: cannyEdges,
-            //    rho: 1, //Distance resolution in pixel-related units
-            //    theta: Math.PI / 360, //Angle resolution measured in radians.
-            //    10, //threshold
-            //    10, //min Line width
-            //    10); //gap between lines
-
             double angle = 0.0;
-
+            //get the average angle
             foreach (LineSegment2D line in lines)
             {
                 angle += Math.Atan2(line.P2.Y - line.P1.Y, line.P2.X - line.P1.X);
@@ -51,12 +44,12 @@ namespace TesseractOCRPlugin__.Net_Standard_.Deskew
 
             // Get the average angle in degrees
             angle = angle / lines.Length * 180.0 / Math.PI;
-
+            //generate image
             Mat rotated = Bilateral;
 
             Image<Bgr, byte> orignalimage = rotated.ToImage<Bgr, byte>(); ;
             Debug.Print("angle detected was: " + angle);
-            //Image<Bgr, byte> rotatedImage = orignalimage.Rotate(angle, new Bgr(255, 255, 255));
+            //Rotate Image
             Image<Bgr, byte> rotatedImage = orignalimage.Rotate(-angle, new Bgr(255, 255, 255));
 
             return rotatedImage.ToBitmap<Bgr, byte>();
